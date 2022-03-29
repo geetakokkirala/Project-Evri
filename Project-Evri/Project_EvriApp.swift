530ba817785b8ca246ffafc06f1378b7a50f3d85
geeta <geetakpb@gmail.com>
2022-03-29 10:38:39 +0100

    Initial Commit

---
+++ Project-Evri/Project_EvriApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_EvriApp.swift
+//  Project-Evri
+//
+//  Created by Admin on 29/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct Project_EvriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
